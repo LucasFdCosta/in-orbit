826ddab5406c95f675dd20fa7cb53f7ff28351b1
Lucas Costa <lucas.co090869@gmail.com>
2024-09-15 12:03:31 -0300

    feat: add create-goal-completion route

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,6 +7,7 @@ import {
 import { createGoal } from "../functions/create-goal";
 import z from "zod";
 import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createGoalCompletion } from "../functions/create-goal-completion";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -40,6 +41,24 @@ app.get("/pending-goals", async () => {
     pendingGoals,
   };
 });
+
+app.post(
+  "/completions",
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body;
+
+    await createGoalCompletion({
+      goalId,
+    });
+  }
+);
 
 app
   .listen({
